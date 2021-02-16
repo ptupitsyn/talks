685cdaf48bd28dc1cc214c8404e14684c4f3b72d
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-02-16 19:50:35 +0300

    wip demos

---
+++ DifferentTakeOnDistributedApplicationsWithApacheIgnite/code/CacheReader/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Apache.Ignite.Core;
+
+using var ignite = Ignition.Start();
+
+var cache = ignite.GetCache<int, Person>("person");
+
+Console.WriteLine(">>>>> Value from cache: " + cache.Get(1));
+Console.ReadKey();
+
+public record Person(string Name, int Age);
---
+++ DifferentTakeOnDistributedApplicationsWithApacheIgnite/code/CacheWriter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Apache.Ignite.Core;
+using Apache.Ignite.Core.Cache.Configuration;
+
+using var ignite = Ignition.Start();
+
+var cfg = new CacheConfiguration
+{
+    Name = "person",
+    CacheMode = CacheMode.Replicated
+};
+
+var cache = ignite.GetOrCreateCache<int, Person>(cfg);
+cache.PutIfAbsent(1, new Person("Ivan", 29));
+
+Console.WriteLine(">>> Value written to cache");
+Console.ReadKey();
+
+public record Person(string Name, int Age);
