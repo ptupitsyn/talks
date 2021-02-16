835cf77c8eeca0225d530760d0209b78e22257b2
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-02-16 19:28:19 +0300

    wip demos

---
+++ DifferentTakeOnDistributedApplicationsWithApacheIgnite/code/Caching/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Apache.Ignite.Core;
+using Apache.Ignite.Core.Cache;
+using Apache.Ignite.Core.Cache.Configuration;
+
+using var ignite = Ignition.Start();
+
+var cfg = new CacheConfiguration
+{
+    Name = "person",
+    CacheMode = CacheMode.Replicated,
+    PlatformCacheConfiguration = new PlatformCacheConfiguration()
+};
+
+var cache = ignite.GetOrCreateCache<int, Person>(cfg);
+
+if (!cache.PutIfAbsent(1, new Person("Ivan", 29)))
+{
+    var res = cache.LocalPeek(1, CachePeekMode.Platform);
+    Console.WriteLine("Value from cache: " + res);
+}
+else
+{
+    Console.WriteLine("Value written to cache");
+}
+
+public record Person(string Name, int Age);
