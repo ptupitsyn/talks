8612d33f64cf756437015ef1c03beaab98a477a6
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-02-16 15:28:07 +0300

    cleanup

--- DifferentTakeOnDistributedApplicationsWithApacheIgnite/code/Benchmarks/Program.cs
+++ DifferentTakeOnDistributedApplicationsWithApacheIgnite/code/Benchmarks/Program.cs
@@ -1,19 +1,4 @@
-﻿using System;
-using BenchmarkDotNet.Running;
-using StackExchange.Redis;
+﻿using BenchmarkDotNet.Running;
+using Benchmarks;
 
-namespace Benchmarks
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("127.0.0.1");
-            //
-            // var db = redis.GetDatabase();
-            // db.StringSet("k", "v");
-
-            BenchmarkRunner.Run<CacheGetRedisIgnite>();
-        }
-    }
-}
+BenchmarkRunner.Run<CacheGetRedisIgnite>();
