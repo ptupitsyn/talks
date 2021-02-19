12beaccbdb528129df2a8ef314719f740c45e6ad
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-02-19 22:15:16 +0300

    wip colocation

--- DifferentTakeOnDistributedApplicationsWithApacheIgnite/code/Colocation/Program.cs
+++ DifferentTakeOnDistributedApplicationsWithApacheIgnite/code/Colocation/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Apache.Ignite.Core;
 using Apache.Ignite.Core.Cache.Affinity;
 using Apache.Ignite.Core.Cache.Configuration;
@@ -14,14 +15,22 @@ using var ignite1 = Ignition.Start(cfg);
 using var ignite2 = Ignition.Start(cfg);
 
 // Start without colocation and show the problem, then fix it.
-var accounts1 = ignite1.GetOrCreateCache<Guid, Account>("account");
-var posts1 = ignite1.GetOrCreateCache<Guid, Post>("post");
+var accountsCache1 = ignite1.GetOrCreateCache<int, Account>("account");
+var postsCache1 = ignite1.GetOrCreateCache<int, Post>("post");
+
+var accountsCache2 = ignite1.GetOrCreateCache<int, Account>("account");
+var postsCache2 = ignite1.GetOrCreateCache<int, Post>("post");
+
+accountsCache1[1] = new Account("Ivan");
+
+for (int i = 0; i < 10; i++)
+    postsCache1[0] = new Post("Text", 1);
 
 
 record Account(string Name);
 
-record Post([property:AffinityKeyMapped]string Text);
+record Post(string Text, int AccountId);
 
 record PostKey(
-    Guid PostId,
-    [property:AffinityKeyMapped] Guid AccountId);
+    int PostId,
+    [property:AffinityKeyMapped] int AccountId);
