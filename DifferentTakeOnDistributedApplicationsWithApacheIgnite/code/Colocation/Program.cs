8e9ea9243d1865af273f7394a5f4ea6c62bef19d
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-02-19 21:37:35 +0300

    wip colocation

---
+++ DifferentTakeOnDistributedApplicationsWithApacheIgnite/code/Colocation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Apache.Ignite.Core;
+using Apache.Ignite.Core.Cache.Affinity;
+using Apache.Ignite.Core.Cache.Configuration;
+using Apache.Ignite.Core.Log;
+
+var cfg = new IgniteConfiguration
+{
+    AutoGenerateIgniteInstanceName = true,
+    Logger = new ConsoleLogger{MinLevel = LogLevel.Error}
+};
+
+using var ignite1 = Ignition.Start(cfg);
+using var ignite2 = Ignition.Start(cfg);
+
+// Start without colocation and show the problem, then fix it.
+var accounts1 = ignite1.GetOrCreateCache<Guid, Account>("account");
+var posts1 = ignite1.GetOrCreateCache<Guid, Post>("post");
+
+
+record Account(string Name);
+
+record Post([property:AffinityKeyMapped]string Text);
+
+record PostKey(
+    Guid PostId,
+    [property:AffinityKeyMapped] Guid AccountId);
