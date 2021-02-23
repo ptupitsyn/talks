4cca39d322b455f75b5a1d9ba01a7d955f9ac4e6
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-02-23 22:25:57 +0300

    wip TimelineUpdate

--- DifferentTakeOnDistributedApplicationsWithApacheIgnite/code/TimelineUpdate/Program.cs
+++ DifferentTakeOnDistributedApplicationsWithApacheIgnite/code/TimelineUpdate/Program.cs
@@ -1,7 +1,11 @@
-﻿using Apache.Ignite.Core;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Apache.Ignite.Core;
+using Apache.Ignite.Core.Cache;
 using Apache.Ignite.Core.Cache.Affinity;
 
-// TODO: Use separate program to demonstrate colocated updates
+// TODO: Use separate program to demonstrate colocated updates?
 using var ignite = Ignition.Start();
 
 record User(string Name);
@@ -12,3 +16,17 @@ record PostKey(
     int PostId,
     [property:AffinityKeyMapped] int UserId);
 
+record Timeline(int UserId, IReadOnlyCollection<PostKey> PostKeys);
+
+class TimelineUpdater : ICacheEntryProcessor<int, Timeline, PostKey, object>
+{
+    public object Process(IMutableCacheEntry<int, Timeline> entry, PostKey arg)
+    {
+        entry.Value = entry.Value with
+        {
+            PostKeys = new List<PostKey>(entry.Value.PostKeys) {arg}
+        };
+
+        return null;
+    }
+}
