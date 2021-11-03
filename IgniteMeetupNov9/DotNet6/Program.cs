168c40c315d70123249fb95a67e097850d231fad
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-11-03 13:30:00 +0300

    tweak records demo

--- IgniteMeetupNov9/DotNet6/Program.cs
+++ IgniteMeetupNov9/DotNet6/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Apache.Ignite.Core;
 using Apache.Ignite.Core.Log;
 
@@ -8,14 +9,19 @@ using var ignite = Ignition.Start(new IgniteConfiguration
 });
 
 var cache = ignite.GetOrCreateCache<EmployeeKey, Employee>("employee");
+var dict = new Dictionary<EmployeeKey, Employee>();
 
 EmployeeKey key1 = new(2, "b-1");
 EmployeeKey key2 = new(2, "b-1");
 
-cache.Put(key1, new("John Doe", new DateTime(2016, 1, 1)));
+Employee employee = new("John Doe", new DateTime(2016, 1, 1));
+
+cache.Put(key1, employee);
+dict.Add(key1, employee);
 
 Console.WriteLine();
 Console.WriteLine($">>> Value from cache: {cache.Get(key2)}");
+Console.WriteLine($">>> Value from dictionary: {dict[key2]}");
 Console.WriteLine($">>> Equals: {key1 == key2}, ReferenceEquals: {ReferenceEquals(key1, key2)}");
 Console.WriteLine();
 
