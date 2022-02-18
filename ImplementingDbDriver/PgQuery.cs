6bfefd4cf1a28e07237b79a61c93020a783cb7ef
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-02-18 14:32:49 +0300

    Add new project

---
+++ ImplementingDbDriver/PgQuery.cs
@@ -0,0 +1,12 @@
+﻿using Npgsql;
+
+using var conn = new NpgsqlConnection("db-host");
+conn.Open();
+
+using var cmd = new NpgsqlCommand("SELECT Name FROM User", conn);
+using var reader = cmd.ExecuteReader();
+
+while (reader.Read())
+{
+    Console.WriteLine(reader.GetString(0));
+}
